2e5ac70c280725347d9ac54eca715d9014fb703c
e.stroganova <e.stroganova@vehikl.com>
2025-05-05 15:55:58 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+<div :style="{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', height: '100vh' }">
+    <button @click='onClick' class="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded m-10">
+    <img src='/vehikl.svg' width='40' height='40'/>Vehikl + Growth
+    </button>
+</div>
+</template>
+<script setup lang="ts">
+const onClick=()=>{
+alert('Let\'s Learn!')
+}
+</script>
