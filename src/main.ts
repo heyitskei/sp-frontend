3b47b785d999600d573c8a197434c2ccaea20797
e.stroganova <e.stroganova@vehikl.com>
2025-05-07 16:28:14 -0400

    introduce prettier & run prettier --fix

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './index.css';
-import { createApp } from 'vue';
-import App from './App.vue';
-createApp(App).mount('#app');
+import "./index.css";
+import { createApp } from "vue";
+import App from "./App.vue";
+createApp(App).mount("#app");
